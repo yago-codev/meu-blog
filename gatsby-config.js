eed3de1f57ddf431081bf13101bea2855d6c1e24
yago-codev <yagomilano92@gmail.com>
2020-04-26 00:20:31 -0300

    Organizando estrutura inicial do projeto

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Yago Milano | Blog`,
+    description: `Dicas e tutoriais sobre desenvolvimento front-end.`,
+    author: `@yagomilano`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
